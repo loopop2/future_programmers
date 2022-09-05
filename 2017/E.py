778b00732e7ab6ba07b9b02e046c904c64c10638
virooos <101967573+virooos@users.noreply.github.com>
2022-09-05 12:47:14 +0300

    Update E.py

--- 2017/E.py
+++ 2017/E.py
@@ -4,13 +4,13 @@ platforms = int(input("Enter the number of platforms: "))
 while platforms < 1 or platforms > 300: platforms = int(input("Enter the number of platforms: "))
 platform_values = [int(input(f"Enter the {x+1} value of your platform: ")) for x in range(platforms)]
 platform_values.sort()
+base = platform_values[0]
 curr_value = platform_values[0]
-jumps = 0
 poped = platform_values.pop(0)
+jumps = 0
 for i in platform_values:
-    if curr_value >= i - 60 and curr_value <= i + 60:
+    if i > curr_value:
         curr_value = i
-
-    if i - 60 < curr_value or i + 60 > curr_value:
+    if curr_value <= base+60:
         jumps+=1
 print(jumps)
