381931585bee9d66e4eeb1d2d4b98f8be73a042d
virooos <101967573+virooos@users.noreply.github.com>
2022-09-12 08:47:43 +0300

    Update E.py

--- 2019/E.py
+++ 2019/E.py
@@ -1,4 +1,6 @@
 n, q = [int(x) for x in input("Enter N and Q: ").split(" ")]
-devices = [int(x) for in input("Devices and Operations: ").split(" ")]
+d_o = [int(x) for in input("Devices and Operations: ").split(" ")]
 while n < 1 or n > 5000: n = int(input("Enter valid N: "))
 while q < 1 or q > pow(10,6): n = int(input("Enter valid Q: "))
+
+untested = [x for x in range(1,len(d_o),2)]
