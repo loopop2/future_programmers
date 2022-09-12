6326e266bccc1a51d36f7eafe9427dea356864e5
virooos <101967573+virooos@users.noreply.github.com>
2022-09-12 08:45:19 +0300

    Create E.py

---
+++ 2019/E.py
@@ -0,0 +1,4 @@
+n, q = [int(x) for x in input("Enter N and Q: ").split(" ")]
+devices = [int(x) for in input("Devices and Operations: ").split(" ")]
+while n < 1 or n > 5000: n = int(input("Enter valid N: "))
+while q < 1 or q > pow(10,6): n = int(input("Enter valid Q: "))
